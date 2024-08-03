0e969a903905f72a019d927e0743fc6b69b95aa6
Adam Cohen <mybirdcansing@gmail.com>
2024-08-02 23:22:34 -0700

    try to fix disable drafts

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -1,22 +1,13 @@
-import { validatePreviewUrl } from '@sanity/preview-url-secret'
-import { readToken } from 'lib/sanity.api'
-import { getClient } from 'lib/sanity.client'
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
-
-const client = getClient({ token: readToken })
 
 export async function GET(req: Request) {
   if (!req.url) {
     throw new Error('Missing url')
   }
-  const { isValid, redirectTo = '/' } = await validatePreviewUrl(
-    client,
-    req.url,
-  )
-  if (!isValid) {
-    return new Response('Invalid slug', { status: 401 })
+  const { isEnabled, disable } = draftMode()
+  if (isEnabled) {
+    disable()
   }
-  draftMode().disable()
-  redirect(redirectTo)
+  redirect(req.referrer ?? '/')
 }
