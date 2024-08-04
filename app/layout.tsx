3b4688a1511c6e0a3cc22911d15e601e344cd100
Adam Cohen <mybirdcansing@gmail.com>
2024-08-04 00:02:51 -0700

    Add lang to html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   const isDraft = draftMode().isEnabled
 
   return (
-    <html>
+    <html lang="en">
       <body>
         <WindowProvider>
           <PlayerProvider>
