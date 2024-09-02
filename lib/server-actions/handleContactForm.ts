3cd308a5b9c5245d2f16c03927ca7aba132cbc08
Adam Cohen <mybirdcansing@gmail.com>
2024-09-01 21:12:25 -0700

    improve error logging for contact form

--- lib/server-actions/handleContactForm.ts
+++ lib/server-actions/handleContactForm.ts
@@ -1,7 +1,19 @@
 'use server'
-// lib/server-actions/handleContactForm.ts
+import { captureException as captureSentryException } from '@sentry/nextjs'
 import nodemailer from 'nodemailer'
 
+export enum RecaptchaErrorCode {
+  'missing-input-secret',
+  'invalid-input-secret',
+  'missing-input-response',
+  'invalid-input-response',
+  'bad-request',
+  'timeout-or-duplicate',
+}
+export interface RecaptchaResult {
+  success: true | false
+  'error-codes'?: RecaptchaErrorCode[]
+}
 export async function handleContactForm(data: FormData) {
   const name = data.get('name')?.toString() || ''
   const email = data.get('email')?.toString() || ''
@@ -11,11 +23,16 @@ export async function handleContactForm(data: FormData) {
   const recaptchaSecret = process.env.RECAPTCHA_SECRET_KEY
   const recaptchaUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${recaptcha}`
 
-  const recaptchaResult = await fetch(recaptchaUrl, { method: 'POST' }).then(
+  const recaptchaResult = (await fetch(recaptchaUrl, { method: 'POST' }).then(
     (res) => res.json(),
-  )
+  )) as RecaptchaResult
 
-  if (!recaptchaResult.success) {
+  if (!recaptchaResult?.success) {
+    const errorCodes = recaptchaResult['error-codes'] ?? []
+
+    captureSentryException(
+      `reCAPTCHA verification failed with error code${errorCodes.length > 1 ? 's' : ''}: ${errorCodes.join(', ')}`,
+    )
     return {
       success: false,
       message: 'reCAPTCHA verification failed. Please try again.',
@@ -38,19 +55,18 @@ export async function handleContactForm(data: FormData) {
   const mailOptions = {
     from: email,
     to: smtpUser,
-    subject: `Contact form submission from ${name}`,
+    subject: `Fan mail from ${name}`,
     text: message,
   }
 
   try {
-    // Send the email
     await transporter.sendMail(mailOptions)
     return {
       success: true,
       message: 'Email sent successfully!',
     }
   } catch (error) {
-    console.error('Error sending email:', error)
+    captureSentryException(`Error sending email: ${error}`)
     return {
       success: false,
       message: 'Failed to send email.',
